da09c07b3d9f76095784a1b95f5ec2cb63b48249
Prescaa <145347108+Prescaa@users.noreply.github.com>
2023-12-16 21:01:07 +0800

    Praktikum Pemrograman I

---
+++ PRAK004_2310817210023_RifkyPutraMahardika.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#   RIFKY PUTRA MAHARDIKA    #\n");
+    printf("#       2310817210023        #\n");
+    printf("#                            #\n");
+    printf("##############################");
+}
