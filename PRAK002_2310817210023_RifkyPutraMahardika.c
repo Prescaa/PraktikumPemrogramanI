606e25b8adbc2c827b281e76afd8c162e626211d
Prescaa <145347108+Prescaa@users.noreply.github.com>
2023-12-16 21:11:22 +0800

    Delete PRAK002_2310817210023_RifkyPutraMahardika.c

--- PRAK002_2310817210023_RifkyPutraMahardika.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main () 
-{
-    printf("Selamat pagi, Rifky Putra Mahardika \n");
-    printf("Selamat siang, Rifky Putra Mahardika \n");
-    printf("Selamat malam, Rifky Putra Mahardika \n");
-    return 0;
-}
